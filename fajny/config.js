0456c2921ea009e1d82d51f814ba8232d9e9788d
mmieciu0077@gmail.com <mmieciu0077@gmail.com>
2018-12-04 17:27:59 +0000

    Initial commit

---
+++ fajny/config.js
@@ -0,0 +1,52 @@
+var CONFIG = {
+  "maps": {
+    "worldco": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1534270952,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 11,
+      "name": "worldco",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "worldco_isometric_t1",
+      "tileSize": 384,
+      "world": "worldco",
+      "worldName": "worldco",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "worldco"
+  ],
+  "tileSetGroups": {
+    "worldco_isometric_t1": {
+      "maxZoom": 111,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
---
+++ super/config.js
@@ -0,0 +1,55 @@
+var CONFIG = {
+  "maps": {
+    "world": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1543941418,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 11,
+      "name": "world",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "world_isometric_t1",
+      "tileSize": [
+        384,
+        384
+      ],
+      "world": "world",
+      "worldName": "world",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "world"
+  ],
+  "tileSetGroups": {
+    "world_isometric_t1": {
+      "maxZoom": 11,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
